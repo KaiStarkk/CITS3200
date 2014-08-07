aba37d1000723478a33d9d391b9f005add61d161
goingpaper <atonkin98@gmail.com>
2014-08-07 16:29:16 +0800

    Updated analyser.java and io.java

--- Project/Source/Algorithms/Analyser.java
+++ Project/Source/Algorithms/Analyser.java
@@ -7,3 +7,20 @@
  * @version 1.0 07/08/14
  * @author Group B
  */
+public class Analyser{
+	
+	private Gps gps;
+	private Time time;
+	private Accelerometers accel;
+	
+	/**
+	 * 
+	 * 
+	 * 
+	 */
+	public Analyser(Dataset dataset){
+		gps = new Gps();
+		time = new Time();
+		accel = new Accelerometers();
+	}
+}
