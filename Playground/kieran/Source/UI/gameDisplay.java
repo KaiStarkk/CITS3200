c360103873b322b7651dd7e6aaf371bd28326a1c
Kieran Hannigan <overyonderstudios@gmail.com>
2014-08-11 15:40:04 +0800

    Move StintEditor to root.

--- Playground/kieran/Source/UI/gameDisplay.java
+++
@@ -1,8 +0,0 @@
-/**
- * CITS3200 Professional Computing
- * gameDisplay.java
- * Purpose: Unsure.
- *
- * @version 1.0 07/08/14
- * @author Group B
- */
--- Playground/kieran/Source/UI/wizard.java
+++
@@ -1,9 +0,0 @@
-/**
- * CITS3200 Professional Computing
- * wizard.java
- * Purpose: Allows the user to set up the initial information. This includes 
- *          choosing the grounds and setting up the match duration periods.
- *
- * @version 1.0 07/08/14
- * @author Group B
- */
