3ac7a5e344ea129cc5014f525677f58a11a40c89
CodeOwl94 <ashwindaniel94@yahoo.com>
2014-08-13 19:21:03 +0800

    Added two links with useful GPS information.

--- Playground/Ashwin/GPSDetection.java
+++ Playground/Ashwin/GPSDetection.java
@@ -9,6 +9,8 @@ public class GPSDetection {
     public static void main(String[] args) {
         
         System.out.println("Hello World!\n");
+        //http://www.gps-data-team.info/whereis/Telstra_Dome
+        //http://www.movable-type.co.uk/scripts/latlong.html
         
     }
 
