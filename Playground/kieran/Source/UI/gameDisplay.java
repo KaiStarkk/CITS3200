451b4bcc33395782ccf441a01bab0ea44fe15969
Kieran Hannigan <overyonderstudios@gmail.com>
2014-08-15 13:03:10 +0800

    Restore my playground folder as a bastion clean project state.

---
+++ Playground/kieran/Source/UI/gameDisplay.java
@@ -0,0 +1,8 @@
+/**
+ * CITS3200 Professional Computing
+ * gameDisplay.java
+ * Purpose: Unsure.
+ *
+ * @version 1.0 07/08/14
+ * @author Group B
+ */
---
+++ Playground/kieran/Source/UI/ui.java
@@ -0,0 +1,8 @@
+/**
+ * CITS3200 Professional Computing
+ * ui.java
+ * Purpose: Sets out the user interface for the program.
+ *
+ * @version 1.0 07/08/14
+ * @author Group B
+ */
