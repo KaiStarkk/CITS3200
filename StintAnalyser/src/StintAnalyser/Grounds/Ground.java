84b22c0aaa7fdca53541a0d4c12f2eec66ab531d
Marcus <20495924@student.uwa.edu.au>
2014-09-08 20:12:21 +0800

    Completed GroundsIO for use in GPS analysis

--- StintAnalyser/src/StintAnalyser/Grounds/Ground.java
+++ StintAnalyser/src/StintAnalyser/Grounds/Ground.java
@@ -8,35 +8,35 @@ package stintanalyser.Grounds;
  * @author Group B
  */
 public class Ground {
-
+    
 	private String name;
 	private GPSCoordinate goal1;
 	private GPSCoordinate goal2;
 	private double aspectRatio;
-
+    
 	public Ground(String name, GPSCoordinate goal1, GPSCoordinate goal2, double aspectRatio) {
 		this.name = name;
 		this.goal1 = goal1;
 		this.goal2 = goal2;
 		this.aspectRatio = aspectRatio;
 	}
-
+    
 	public double getTransformBearing() {
 		return this.goal1.bearingTo(this.goal2);
 	}
-
+    
 	public double getAspectRatio() {
 		return this.aspectRatio;
 	}
-
+    
 	public double getFieldLength() {
 		return this.goal1.distanceTo(this.goal2);
 	}
-
+    
 	public double getHalfFieldWidth() {
 		return (this.getFieldLength() * this.getAspectRatio()) / 2;
 	}
-
+    
 	public GPSCoordinate getOrigin() {
 		return this.goal1;
 	}
---
+++ StintAnalyser/src/stintanalyser/Grounds/GroundsIO.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package stintanalyser.Grounds;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author Markcuz
+ */
+public class GroundsIO {
+    
+    List<String[]> groundsList;
+    
+    public GroundsIO(String pathName) {
+        groundsList = new ArrayList<>();
+        loadGrounds(pathName);  
+    }
+    
+    /**
+     * opens the grounds txt file and adds to the list
+     * @param pathName 
+     */
+    final public void loadGrounds(String pathName) {
+        try(BufferedReader br = new BufferedReader(new FileReader(pathName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] current = line.split("\\t");
+
+                boolean add = groundsList.add(current);
+            }
+        }
+        catch (IOException e){
+            System.err.println("Could not open file.");
+        }
+    }
+    
+    /**
+     * returns the list for grounds
+     * @return groundsList
+     */
+    public final List<String[]> getGroundsDB() {
+        return groundsList;
+    }
+    
+    /**
+     * Prints a database to standard output.
+     */
+    public final void printDB() {
+        List<String[]> temp = groundsList;
+        if (!temp.isEmpty()) {
+            temp.stream().forEach((entry) -> {
+                System.out.println(Arrays.toString(entry));
+            });
+            System.out.println();
+        } else {
+            System.out.println("Grounds database is empty.\n");
+        }
+    }
+    
+    /**
+     * selects the given ground and constructs the Ground
+     * @param name
+     * @return the ground required
+     */
+    public Ground chooseGround(String name) {
+        
+        int i = 0;
+        
+        if(i!=groundsList.size()&&(!groundsList.isEmpty())) {
+            while(!groundsList.get(i)[8].equals(name)) {
+                i++;
+            }
+        }
+        else {
+            System.out.println("Could not find ground"+name);
+        }
+        
+        GPSCoordinate gps1 = new GPSCoordinate(Double.parseDouble(groundsList.get(i)[0]), Double.parseDouble(groundsList.get(i)[1]));
+        
+        GPSCoordinate gps2 = new GPSCoordinate(Double.parseDouble(groundsList.get(i)[3]), Double.parseDouble(groundsList.get(i)[4]));
+       
+        double aspectRatio = Double.parseDouble(groundsList.get(i)[6]);
+        
+        Ground ground = new Ground(name, gps1, gps2, aspectRatio);
+        
+        return ground;  
+    }
+    
+    /**
+     * outputs an array of strings for use in UI
+     * @return array of groundNames
+     */
+    public final String[] groundNames() {
+        
+        String[] out = new String[groundsList.size()];
+        
+        if (!groundsList.isEmpty()) {
+            for(int i = 0; i<groundsList.size(); i++) {
+                out[i] = groundsList.get(i)[8];
+            } 
+        } 
+        else {
+            System.out.println("Grounds database is empty.\n");
+        }
+        
+        return out;
+    }
+    
+}
