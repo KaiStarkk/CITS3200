82394d8f072339688e644043bf86147c38a9fda8
Kieran Hannigan <overyonderstudios@gmail.com>
2014-10-15 12:16:08 +0800

    Make game time input format change. Make project natively packagable.

--- StintAnalyser/src/StintAnalyser/Main.java
+++ StintAnalyser/src/StintAnalyser/Main.java
@@ -34,6 +34,18 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.setTitle("StintAnalyser");
         primaryStage.show();
+    }    
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
     }
-    
+
 }
